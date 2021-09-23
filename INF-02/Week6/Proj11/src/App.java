bbc74e5bd02b619ec5e87575877a697478d8f5cf
JRF344 <Joshrf344@gmail.com>
2021-09-22 18:07:22 -0600

    Backlog from last courses

---
+++ INF-02/Week6/Proj11/src/App.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Factorial {
+
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintStream out = System.out;
+
+
+    public static void main(String[] args) throws Exception {
+        int students;
+        double average;
+
+        out.print("Digite la cantidad de estudiantes en el curso");
+        students = Integer.parseInt(in.readLine());
+
+        while (i < students) {
+            out.print("Digite el promedio del estudiante " + i);
+            
+        }
+
+        out.print("El factorial de N! ("+n+"!) es: " + factorial);
+
+    }
+}
